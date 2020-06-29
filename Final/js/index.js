c35e218413daccd21f65f535add7f7cb1c0944e6
Alessio Scoccia <alesko95@gmail.com>
2020-06-29 13:10:48 +0200

    fix about e aggiunte foto e video da tracciare

--- Final/js/index.js
+++ Final/js/index.js
@@ -1,30 +1,30 @@
-var a = 0;
-$(window).scroll(function () {
-	var oTop = $('#counter-div').offset().top() - window.innerHeight;
-	if (a == 0 && $(window).scrollTop() > oTop) {
-		$('.count').each(function () {
-			var $this = $(this),
-				countTo = $this.attr('data-count');
-			$({
-				countNum: $this.text()
-			}).animate({
-				countNum: countTo
-			}, {
-				duration: 2000,
-				easing: 'swing',
-				step: function () {
-					$this.text(Math.floor(this.countNum));
-				},
-				complete: function () {
-					$this.text(this.countNum);
-					//alert('finished');
-				}
+// var a = 0;
+// $(window).scroll(function () {
+// 	var oTop = $('#counter-div').offset().top() - window.innerHeight;
+// 	if (a == 0 && $(window).scrollTop() > oTop) {
+// 		$('.count').each(function () {
+// 			var $this = $(this),
+// 				countTo = $this.attr('data-count');
+// 			$({
+// 				countNum: $this.text()
+// 			}).animate({
+// 				countNum: countTo
+// 			}, {
+// 				duration: 2000,
+// 				easing: 'swing',
+// 				step: function () {
+// 					$this.text(Math.floor(this.countNum));
+// 				},
+// 				complete: function () {
+// 					$this.text(this.countNum);
+// 					//alert('finished');
+// 				}
 
-			});
-		});
-		a = 1;
-	}
-});
+// 			});
+// 		});
+// 		a = 1;
+// 	}
+// });
 
 var map = new ol.Map({
 	target: 'map',
